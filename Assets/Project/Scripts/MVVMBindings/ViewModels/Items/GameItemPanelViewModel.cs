7a8b1dbe5693d8731a3d349601c8df8530c204cd
Jerome Ghesquiere <jerome.ghesquiere@gmail.com>
2020-04-28 17:35:22 +0200

    Remove unused code

--- Assets/Project/Scripts/MVVMBindings/ViewModels/Items/GameItemPanelViewModel.cs
+++ Assets/Project/Scripts/MVVMBindings/ViewModels/Items/GameItemPanelViewModel.cs
@@ -41,34 +41,6 @@ public class GameItemPanelViewModel : MainViewModel
 
             process.WaitForExit();
             process.Close();
-
-
-            List<System.Diagnostics.Process> procs = new List<System.Diagnostics.Process>(System.Diagnostics.Process.GetProcesses());
-            System.Diagnostics.Process steam = procs.Find(x => x.ProcessName == "Steam");
-            int steamId = steam.Id;
-
-            foreach (var proc in procs)
-            {
-                using (var mo = new System.Management.ManagementObject($"win32_process.handle='{proc.Id}'"))
-                {
-                    if (mo != null)
-                    {
-                        try
-                        {
-                            mo.Get();
-                            int parentPid = Convert.ToInt32(mo["ParentProcessId"]);
-                            if (parentPid == steamId)
-                            {
-                                Debug.Log($"{proc.ProcessName} is running as a child to {mo["ParentProcessId"]}");
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            // the process ended between polling all of the procs and requesting the management object
-                        }
-                    }
-                }
-            }
         }
     }
 }
